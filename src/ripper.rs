cb132f802e6585ed2de1782aebd5e01a844ea9b8
Jos Dehaes <jos.dehaes@gmail.com>
2022-04-24 23:27:25 +0200

    add id3, cleanup

--- src/ripper.rs
+++ src/ripper.rs
@@ -3,31 +3,18 @@ use gstreamer::*;
 
 pub fn extract() {
     gstreamer::init().unwrap();
-    let location = "/home/jos/Downloads/file_example_WAV_1MG.wav";
-    // let queue = ElementFactory::make("queue", None).unwrap();
     // let extractor = Element::make_from_uri(URIType::Src, "cdda://1", Some("cd_src")).unwrap();
     let file = ElementFactory::make("filesrc", None).unwrap();
-    file.set_property("location", location);
+    file.set_property("location", "/home/jos/Downloads/file_example_WAV_1MG.wav");
     let wav = ElementFactory::make("wavparse", None).unwrap();
     let encoder = ElementFactory::make("lamemp3enc", None).unwrap();
-    // let encoder = parse_bin_from_description("lamemp3enc ! id3v2mux", false).unwrap();
+    let id3 = ElementFactory::make("id3v2mux", None).unwrap();
     let sink = ElementFactory::make("filesink", None).unwrap();
     sink.set_property("location", "/home/jos/Downloads/file_example_WAV_1MG.mp3");
     let pipeline = Pipeline::new(Some("ripper"));
-    pipeline.add(&file).unwrap();
-    pipeline.add(&wav).unwrap();
-    pipeline.add(&encoder).unwrap();
-    pipeline.add(&sink).unwrap();
-    Element::link_many(&[&file, &wav, &encoder, &sink]).unwrap();
-
-    // let pipeline_weak = pipeline.downgrade();
-    // wav.connect_pad_added(move |dbin, src_pad| {
-    //     let pipeline = match pipeline_weak.upgrade() {
-    //         Some(pipeline) => pipeline,
-    //         None => return,
-    //     };
-    //     println!("padd added");
-    // });
+    let elements = &[&file, &wav, &encoder, &id3, &sink];
+    pipeline.add_many(elements).unwrap();
+    Element::link_many(elements).unwrap();
 
     pipeline.set_state(State::Playing).unwrap();
 
@@ -37,7 +24,11 @@ pub fn extract() {
 
     for msg in bus.iter_timed(gstreamer::ClockTime::NONE) {
         match msg.view() {
-            MessageView::Eos(..) => break,
+            MessageView::Eos(..) => {
+                pipeline.set_state(State::Null).unwrap();
+                println!("done");
+                break;
+            },
             MessageView::Error(err) => {
                 pipeline.set_state(State::Null).unwrap();
                 println!(
