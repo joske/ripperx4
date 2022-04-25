03476f32b061d71b7fd3f493db63e8395cb46879
Jos Dehaes <jos.dehaes@gmail.com>
2022-04-25 22:56:30 +0200

    actually does something

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::path::Path;
 use std::thread;
 
 use glib::clone;
-use gtk::Builder;
-use gtk::ResponseType;
-use gtk::Window;
 use gtk::ffi::GtkBuilder;
 use gtk::prelude::*;
 use gtk::Application;
 use gtk::ApplicationWindow;
+use gtk::Builder;
 use gtk::Button;
 use gtk::Dialog;
+use gtk::Image;
+use gtk::Statusbar;
+use gtk::Window;
 use ripper::extract;
 
 mod ripper;
@@ -28,21 +30,44 @@ pub fn main() {
 }
 
 fn build_ui(app: &Application) {
-    let builder = Builder::from_resource("ripperx4.ui");
-    // Create a button with label and margins
-    let exitButton : Button = builder.object("exit").unwrap();
-    let goButton : Button = builder.object("exit").unwrap();
+    let builder = Builder::new();
+    builder.add_from_file(Path::new("ripperx4.ui")).ok();
+    let window: Window = builder.object("window").unwrap();
+    window.set_application(Some(app));
+    window.present();
 
-    // let window: ApplicationWindow =ApplicationWindow::builder().application(app).build();
-    goButton.connect_clicked(move |_| {
+    let exit_button: Button = builder.object("exit").unwrap();
+    let image = Image::from_file("src/xpms/exit.xpm");
+    exit_button.set_child(Some(&image));
+    exit_button.connect_clicked(move |_| {
+        window.close();
+    });
+
+    let go_button: Button = builder.object("go_button").unwrap();
+    let image = Image::from_file("src/xpms/go.xpm");
+    go_button.set_child(Some(&image));
+    go_button.connect_clicked(move |_| {
+        let status: Statusbar = builder.object("statusbar").unwrap();
+        let context_id = status.context_id("foo");
+        let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
         thread::spawn(move || {
+            let _ = tx.send(Some(1));
             extract();
+            println!("done");
+            let _ = tx.send(None);
+        });
+        rx.attach(None, move |value| match value {
+            Some(value) => {
+                status.push(context_id, "starting");
+                glib::Continue(true)
+            }
+            None => {
+                println!("received done");
+                status.pop(context_id);
+                status.push(context_id, "done");
+                glib::Continue(false)
+            }
         });
     });
 
-    // Create a window
-    let window : Window = builder.object("window").unwrap();
-
-    // Present window
-    window.present();
 }
