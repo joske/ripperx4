b1ec0fef5ae1895e47dd2e81081be67eb9b12535
Jos Dehaes <jos.dehaes@gmail.com>
2023-01-19 22:12:34 +0100

    make it build on macos: libdiscid

--- build.rs
+++ build.rs
@@ -6,4 +6,6 @@ fn main() {
         "content/ripperx4.gresource.xml",
         "ripperx4.gresource",
     );
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-L/opt/homebrew/lib/");
 }
