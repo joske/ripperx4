9235dcf985b842744d044aacb2e4e70e79552ab0
Jos Dehaes <jos.dehaes@gmail.com>
2026-01-12 15:46:21 +0100

    use pkg-config to find libs on macOS

--- build.rs
+++ build.rs
@@ -6,6 +6,4 @@ fn main() {
         "content/ripperx4.gresource.xml",
         "ripperx4.gresource",
     );
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-L/opt/homebrew/lib/");
 }
